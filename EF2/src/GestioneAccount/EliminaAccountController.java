ded6032995571695096d67c6a5f02c947995c370
Lucailardi <luca.ilardi01@universitadipavia.it>
2024-03-01 01:22:13 +0100

    modifica diagrammi di sequenza

--- EF2/src/GestioneAccount/EliminaAccountController.java
+++ EF2/src/GestioneAccount/EliminaAccountController.java
@@ -56,6 +56,7 @@ public class EliminaAccountController {
     					view.getErroreText().setText("Eliminazione dell'account avvenuta con successo"); 
     					model.eliminaAccount();
     					view.dispose();
+    					//IL MODEL DEVE CHIAMARE
     					new BigFrameController(new BigFrameView(), new BigFrameModel());
                         
     				} else {
